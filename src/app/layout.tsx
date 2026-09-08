ad4f72ec8f08e0c2e5b9ab4763e0ca024cfd8241
fakhrurrizal10 <fakhrurrizal10@gmail.com>
2026-09-08 16:30:43 +0700

    Update ci cd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
+import type { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return children;
 }
