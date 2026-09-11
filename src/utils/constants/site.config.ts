69f796c15da6ba43df6bc9dfe91a6d634d91d24f
fakhrurrizal10 <fakhrurrizal10@gmail.com>
2026-09-11 10:51:10 +0700

    Update tampilan article

--- src/utils/constants/site.config.ts
+++ src/utils/constants/site.config.ts
@@ -1,3 +1,3 @@
 export const FOUNDING_YEAR = 2001;
 export const YEARS_OF_EXPERIENCE = new Date().getFullYear() - FOUNDING_YEAR;
-export const YEARS_DISPLAY = `${YEARS_OF_EXPERIENCE}+`;
+export const YEARS_DISPLAY = `${YEARS_OF_EXPERIENCE}`;
